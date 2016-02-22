891169903344a184dc3bbc6c6713661d6c12126b
ccLzz <870677775@qq.com>
2016-02-22 12:06:18 +0800

    2.22 12.03

---
+++ 易牧售后/ImageNames.h
@@ -0,0 +1,23 @@
+//
+//  ImageNames.h
+//  易牧售后
+//
+//  Created by 软通 on 16/2/22.
+//  Copyright © 2016年 北京易牧科技有限公司. All rights reserved.
+//
+
+#ifndef tongle_TLImageName_h
+#define tongle_TLImageName_h
+
+
+//首页按下图标
+#define  TL_MAIN_PRESS  @"main_press"
+//收藏按下图标
+#define  TL_COLLECT_PRESS @"collect_press"
+
+
+
+
+
+
+#endif
---
+++ 易牧售后/Urls.h
@@ -0,0 +1,13 @@
+//
+//  Urls.h
+//  易牧售后
+//
+//  Created by 软通 on 16/2/22.
+//  Copyright © 2016年 北京易牧科技有限公司. All rights reserved.
+//
+
+#ifndef Urls_h
+#define Urls_h
+
+
+#endif /* Urls_h */
